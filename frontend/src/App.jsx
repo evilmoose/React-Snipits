0adbd4072aa2869d2bcc4d0c13dd63b2be1760cd
Evilmoose <ronnielopez@gmail.com>
2023-05-23 14:16:32 -0700

    useEffect Object example

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    const [name, setName] = useState('peter');
+    const [age, setAge] = useState(24);
+    const [hobby, setHobby] = useState('read books');
+
+    const displayPerson = () => {
+      setName('john');
+      setAge(28);
+      setHobby('scream at the computer');
+    }
+
+    return (
+      <>
+        <h3>{name}</h3>
+        <h3>{age}</h3>
+        <h4>Enjoys to: {hobby}</h4>
+        <button className='btn' onClick={displayPerson}>
+          show john
+        </button>
+      </>
+    );
+}
+
+export default App
